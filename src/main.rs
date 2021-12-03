d4cf5eeb0d79737ebe72e236c65c651cc48f09ef
ThiFerNe <thimo.f.neumann@gmail.com>
2021-12-03 19:29:48 +0100

    Add basic command line parser structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::{crate_authors, crate_description, crate_version, App, ArgMatches};
+
+use thiserror::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Advent of Code 2021")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .get_matches();
+    if let Err(error) = handle_matches(matches) {
+        eprintln!("Error: {}", error);
+    }
+}
+
+fn handle_matches(matches: ArgMatches) -> Result<(), HandleMatchesError> {
+    match matches.subcommand() {
+        (subcommand_name, _) => Err(HandleMatchesError::SubCommandDoesNotExist(
+            subcommand_name.to_string(),
+        )),
+    }
+}
+
+#[derive(Debug, Error)]
+enum HandleMatchesError {
+    #[error("SubCommand \"{0}\" does not exist")]
+    SubCommandDoesNotExist(String),
 }
