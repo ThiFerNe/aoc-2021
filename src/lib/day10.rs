a74fcfb75cd9531c47855d2fc4e608d104cae69a
ThiFerNe <thimo.f.neumann@gmail.com>
2021-12-10 12:28:09 +0100

    Add solution of day 10 for second puzzle

--- src/lib/day10.rs
+++ src/lib/day10.rs
@@ -2,7 +2,7 @@ use clap::{App, Arg, ArgMatches, SubCommand};
 
 use thiserror::Error;
 
-use super::{read_file_contents, ReadFileContentsError};
+use super::{clap_arg_puzzle_part_time_two, read_file_contents, ReadFileContentsError};
 
 pub const SUBCOMMAND_NAME: &str = "day10";
 
@@ -17,17 +17,30 @@ pub fn subcommand() -> App<'static, 'static> {
                 .help("sets the input file")
                 .default_value("puzzle-inputs/day10-input"),
         )
+        .arg(clap_arg_puzzle_part_time_two())
 }
 
 pub fn handle(matches: &ArgMatches) -> Result<(), Day10Error> {
     let input_file = matches.value_of("input_file");
     let file_contents = read_file_contents(input_file)
         .map_err(|error| Day10Error::ReadFileContents(input_file.map(str::to_string), error))?;
-    let total_syntax_error_score = calculate_total_syntax_error_score(&file_contents)?;
-    println!(
-        "The total syntax error socre is: {}.",
-        total_syntax_error_score
-    );
+    match matches.value_of("puzzle_part").unwrap_or("two") {
+        "two" | "2" => {
+            let middle_autocomplete_score = calculate_middle_autocomplete_score(&file_contents)?;
+            println!(
+                "The middle autocomplete score is: {}.",
+                middle_autocomplete_score
+            );
+        }
+        _ => {
+            let total_syntax_error_score = calculate_total_syntax_error_score(&file_contents)?;
+            println!(
+                "The total syntax error score is: {}.",
+                total_syntax_error_score
+            );
+        }
+    };
+
     Ok(())
 }
 
@@ -37,19 +50,94 @@ pub enum Day10Error {
     ReadFileContents(Option<String>, #[source] ReadFileContentsError),
     #[error("Could not calculate total syntax error score ({0})")]
     CalculateTotalSyntaxErrorScore(#[from] CalculateTotalSyntaxErrorScoreError),
+    #[error("Could not calculate total autocomplete score ({0})")]
+    CalculateTotalAutocompleteScore(#[from] CalculateMiddleAutocompleteScoreError),
 }
 
 pub fn calculate_total_syntax_error_score(
     navigation_subsystem: &str,
 ) -> Result<u128, CalculateTotalSyntaxErrorScoreError> {
-    Ok(navigation_subsystem
+    Ok(parse_navigation_subsystem_lines(navigation_subsystem)
+        .into_iter()
+        .filter_map(Result::err)
+        .map(|error| match error {
+            ParseNavigationSubsystemLineError::ExpectedButFound(_, found_symbol, _, _) => {
+                Ok(match found_symbol {
+                    SyntaxSymbol::Parentheses(_) => 3,
+                    SyntaxSymbol::Brackets(_) => 57,
+                    SyntaxSymbol::Braces(_) => 1197,
+                    SyntaxSymbol::AngleBrackets(_) => 25137,
+                })
+            }
+            _ => Err(CalculateTotalSyntaxErrorScoreError::ParseNavigationSubsystemLine(error)),
+        })
+        .collect::<Result<Vec<u128>, CalculateTotalSyntaxErrorScoreError>>()?
+        .into_iter()
+        .sum::<u128>())
+}
+
+#[derive(Debug, Error, Eq, PartialEq)]
+pub enum CalculateTotalSyntaxErrorScoreError {
+    #[error("Could not parse navigation subsystem line ({0})")]
+    ParseNavigationSubsystemLine(#[source] ParseNavigationSubsystemLineError),
+}
+
+pub fn calculate_middle_autocomplete_score(
+    navigation_subsystem: &str,
+) -> Result<u128, CalculateMiddleAutocompleteScoreError> {
+    let mut autocompletion_score_per_line = parse_navigation_subsystem_lines(navigation_subsystem)
+        .into_iter()
+        .map(|result| match result {
+            Ok(_) | Err(ParseNavigationSubsystemLineError::ExpectedButFound(_, _, _, _)) => {
+                Ok(result)
+            }
+            Err(error) => Err(error),
+        })
+        .collect::<Result<
+            Vec<Result<ParsedLine, ParseNavigationSubsystemLineError>>,
+            ParseNavigationSubsystemLineError,
+        >>()?
+        .into_iter()
+        .filter_map(Result::ok)
+        .map(|parsed_line: ParsedLine| {
+            let mut score = 0;
+            for current_symbol in parsed_line.missing_closing_symbols_stack.iter().rev() {
+                score *= 5;
+                score += match current_symbol {
+                    SyntaxSymbol::Parentheses(_) => 1,
+                    SyntaxSymbol::Brackets(_) => 2,
+                    SyntaxSymbol::Braces(_) => 3,
+                    SyntaxSymbol::AngleBrackets(_) => 4,
+                };
+            }
+            score
+        })
+        .collect::<Vec<u128>>();
+    autocompletion_score_per_line.sort_unstable();
+    Ok(if autocompletion_score_per_line.len() % 2 == 0 {
+        (autocompletion_score_per_line[autocompletion_score_per_line.len() / 2]
+            + autocompletion_score_per_line[autocompletion_score_per_line.len() / 3])
+            / 2
+    } else {
+        autocompletion_score_per_line[autocompletion_score_per_line.len() / 2]
+    })
+}
+
+#[derive(Debug, Error, Eq, PartialEq)]
+pub enum CalculateMiddleAutocompleteScoreError {
+    #[error("Could not parse navigation subsystem line ({0})")]
+    ParseNavigationSubsystemLine(#[from] ParseNavigationSubsystemLineError),
+}
+
+fn parse_navigation_subsystem_lines(
+    navigation_subsystem: &str,
+) -> Vec<Result<ParsedLine, ParseNavigationSubsystemLineError>> {
+    navigation_subsystem
         .lines()
         .enumerate()
         .map(|(lines_index, line)| {
             if line.trim().is_empty() {
-                Err(CalculateTotalSyntaxErrorScoreError::LineIsEmpty(
-                    lines_index,
-                ))
+                Err(ParseNavigationSubsystemLineError::LineIsEmpty(lines_index))
             } else {
                 let mut chunk_stack = Vec::new();
                 for (line_index, current_character) in line.chars().enumerate() {
@@ -59,7 +147,7 @@ pub fn calculate_total_syntax_error_score(
                     } else if let Some(opening_symbol) = chunk_stack.pop() {
                         let expected_closing_character = opening_symbol.closing_variation();
                         if expected_closing_character != current_symbol {
-                            return Err(CalculateTotalSyntaxErrorScoreError::ExpectedButFound(
+                            return Err(ParseNavigationSubsystemLineError::ExpectedButFound(
                                 expected_closing_character,
                                 current_symbol,
                                 lines_index,
@@ -67,36 +155,37 @@ pub fn calculate_total_syntax_error_score(
                             ));
                         }
                     } else {
-                        return Err(CalculateTotalSyntaxErrorScoreError::NotExpectedButFound(
+                        return Err(ParseNavigationSubsystemLineError::NotExpectedButFound(
                             current_symbol,
                             lines_index,
                             line_index,
                         ));
                     }
                 }
-                Ok(())
+                Ok(ParsedLine::of(&chunk_stack))
             }
         })
-        .filter(Result::is_err)
-        .map(Result::unwrap_err)
-        .map(|error: CalculateTotalSyntaxErrorScoreError| match error {
-            CalculateTotalSyntaxErrorScoreError::ExpectedButFound(_, found_symbol, _, _) => {
-                Ok(match found_symbol {
-                    SyntaxSymbol::Parentheses(_) => 3,
-                    SyntaxSymbol::Brackets(_) => 57,
-                    SyntaxSymbol::Braces(_) => 1197,
-                    SyntaxSymbol::AngleBrackets(_) => 25137,
-                })
-            }
-            _ => Err(error),
-        })
-        .collect::<Result<Vec<u128>, CalculateTotalSyntaxErrorScoreError>>()?
-        .into_iter()
-        .sum::<u128>())
+        .collect()
+}
+
+#[derive(Debug)]
+struct ParsedLine {
+    missing_closing_symbols_stack: Vec<SyntaxSymbol>,
+}
+
+impl ParsedLine {
+    fn of(opened_symbols_stack: &[SyntaxSymbol]) -> Self {
+        Self {
+            missing_closing_symbols_stack: opened_symbols_stack
+                .iter()
+                .map(SyntaxSymbol::closing_variation)
+                .collect(),
+        }
+    }
 }
 
 #[derive(Debug, Error, Eq, PartialEq)]
-pub enum CalculateTotalSyntaxErrorScoreError {
+pub enum ParseNavigationSubsystemLineError {
     #[error("Line no. {0} is empty")]
     LineIsEmpty(usize),
     #[error("Could not parse a character ({0})")]
@@ -106,6 +195,7 @@ pub enum CalculateTotalSyntaxErrorScoreError {
     #[error("Expected nothing, but found {0:?} instead (at line {1} index {2})")]
     NotExpectedButFound(SyntaxSymbol, usize, usize),
 }
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum SyntaxSymbol {
     Parentheses(SymbolVariation),   // (
@@ -180,5 +270,21 @@ mod tests {
 
         // then
         assert_eq!(total_syntax_error_score, Ok(26397));
+    }
+
+    #[test]
+    fn calculate_total_syntax_error_score_should_return_288957() {
+        // given
+        let input = "[({(<(())[]>[[{[]{<()<>>\r\n[(()[<>])]({[<{<<[]>>(\r\n\
+                            {([(<{}[<>[]}>{[]{[(<()>\r\n(((({<>}<{<{<>}{[]{[]{}\r\n\
+                            [[<[([]))<([[{}[[()]]]\r\n[{[{({}]{}}([{[{{{}}([]\r\n\
+                            {<[[]]>}<{[{[{[]{()[[[]\r\n[<(<(<(<{}))><([]([]()\r\n\
+                            <{([([[(<>()){}]>(<<{{\r\n<{([{{}}[<[[[<>{}]]]>[]]";
+
+        // when
+        let middle_autocomplete_score = calculate_middle_autocomplete_score(input);
+
+        // then
+        assert_eq!(middle_autocomplete_score, Ok(288957));
     }
 }
