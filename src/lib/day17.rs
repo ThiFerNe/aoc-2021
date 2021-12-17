a839bac8b4f67690b8677fba466fe9c36c744adc
ThiFerNe <thimo.f.neumann@gmail.com>
2021-12-17 15:17:50 +0100

    Add solution of day 17 for second puzzle

--- src/lib/day17.rs
+++ src/lib/day17.rs
@@ -5,7 +5,7 @@ use clap::{App, Arg, ArgMatches, SubCommand};
 
 use thiserror::Error;
 
-use super::{read_file_contents, ReadFileContentsError};
+use super::{clap_arg_puzzle_part_time_two, read_file_contents, ReadFileContentsError};
 
 pub const SUBCOMMAND_NAME: &str = "day17";
 
@@ -20,17 +20,30 @@ pub fn subcommand() -> App<'static, 'static> {
                 .help("sets the input file")
                 .default_value("puzzle-inputs/day17-input"),
         )
+        .arg(clap_arg_puzzle_part_time_two())
 }
 
 pub fn handle(matches: &ArgMatches) -> Result<(), Day17Error> {
     let input_file = matches.value_of("input_file");
     let file_contents = read_file_contents(input_file)
         .map_err(|error| Day17Error::ReadFileContents(input_file.map(str::to_string), error))?;
-    let highest_y_position_possible = find_highest_y_position_possible(&file_contents)?;
-    println!(
-        "The highest y position possible is {}.",
-        highest_y_position_possible
-    );
+    match matches.value_of("puzzle_part").unwrap_or("two") {
+        "two" | "2" => {
+            let count_of_distinct_initial_velocities =
+                count_distinct_initial_velocities(&file_contents)?;
+            println!(
+                "There are {} distinct initial velocity values causing the probe to be within the target area after any step.",
+                count_of_distinct_initial_velocities
+            );
+        }
+        _ => {
+            let highest_y_position_possible = find_highest_y_position_possible(&file_contents)?;
+            println!(
+                "The highest y position possible is {}.",
+                highest_y_position_possible
+            );
+        }
+    };
     Ok(())
 }
 
@@ -38,28 +51,47 @@ pub fn handle(matches: &ArgMatches) -> Result<(), Day17Error> {
 pub enum Day17Error {
     #[error("Could not read file contents of \"{0:?}\" ({1})")]
     ReadFileContents(Option<String>, #[source] ReadFileContentsError),
+    #[error("Could not count distinct initial velocities ({0})")]
+    CountDistinctInitialVelocities(#[from] CountDistinctInitialVelocitiesError),
     #[error("Could not find highest y position possible ({0})")]
     FindHighestYPositionPossible(#[from] FindHighestYPositionPossibleError),
+}
+
+pub fn count_distinct_initial_velocities(
+    input_target_area: &str,
+) -> Result<usize, CountDistinctInitialVelocitiesError> {
+    let target_area = TargetArea::from_str(input_target_area)?;
+    Ok(get_all_p(&target_area)
+        .into_iter()
+        .fold(Vec::new(), |mut distinct, simulated_shot| {
+            if !distinct.contains(&simulated_shot) {
+                distinct.push(simulated_shot);
+            }
+            distinct
+        })
+        .len())
+}
+
+#[derive(Debug, Error, Eq, PartialEq)]
+pub enum CountDistinctInitialVelocitiesError {
+    #[error("Could not parse target area from string ({0})")]
+    TargetAreaFromStr(#[from] TargetAreaFromStrError),
 }
 
 pub fn find_highest_y_position_possible(
     input_target_area: &str,
 ) -> Result<i128, FindHighestYPositionPossibleError> {
     let target_area = TargetArea::from_str(input_target_area)?;
-    (0..=target_area.max_x)
-        .flat_map(|x| {
-            (0..=(target_area.min_y.abs().max(target_area.max_x) * 2))
-                .map(move |y| Velocity { x, y })
-        })
-        .filter_map(|velocity| simulate_shot(&Position { x: 0, y: 0 }, &velocity, &target_area))
+    get_all_p(&target_area)
+        .into_iter()
         .reduce(|a, b| {
-            if a.highest_y_position_reached > b.highest_y_position_reached {
+            if a.highest_y_position_reached() > b.highest_y_position_reached() {
                 a
             } else {
                 b
             }
         })
-        .map(|simulated_shot| simulated_shot.highest_y_position_reached)
+        .map(|simulated_shot| simulated_shot.highest_y_position_reached())
         .ok_or(FindHighestYPositionPossibleError::UnableToFind)
 }
 
@@ -71,33 +103,62 @@ pub enum FindHighestYPositionPossibleError {
     UnableToFind,
 }
 
+fn get_all_p(target_area: &TargetArea) -> Vec<SimulatedShot> {
+    get_all(
+        0,
+        target_area.max_x,
+        -target_area.min_y.abs(),
+        target_area.min_y.abs(),
+        &Position { x: 0, y: 0 },
+        target_area,
+    )
+}
+
+fn get_all(
+    min_x: i128,
+    max_x: i128,
+    min_y: i128,
+    max_y: i128,
+    initial_position: &Position,
+    target_area: &TargetArea,
+) -> Vec<SimulatedShot> {
+    (min_x..=max_x)
+        .flat_map(|x| (min_y..=max_y).map(move |y| Velocity { x, y }))
+        .map(|velocity| simulate_shot(initial_position, &velocity, target_area))
+        .filter(SimulatedShot::was_successful)
+        .collect()
+}
+
 fn simulate_shot(
     start_position: &Position,
     initial_velocity: &Velocity,
     target_area: &TargetArea,
-) -> Option<SimulatedShot> {
+) -> SimulatedShot {
     let mut current_position = *start_position;
     let mut current_velocity = *initial_velocity;
-    let mut step = 0;
-    let mut highest_y_reached = current_position.y;
-    while current_position.x < target_area.max_x && current_position.y > target_area.max_y {
+    let mut mid_positions = Vec::new();
+    while current_position.x <= target_area.max_x
+        && current_position.y >= target_area.min_y
+        && !target_area.contains(&current_position)
+    {
+        if current_position != *start_position {
+            mid_positions.push(current_position);
+        }
         current_position.x += current_velocity.x;
         current_position.y += current_velocity.y;
         current_velocity.x -= current_velocity.x.signum();
         current_velocity.y -= 1;
-        step += 1;
-        if current_position.y > highest_y_reached {
-            highest_y_reached = current_position.y;
-        }
     }
-    if target_area.contains(&current_position) {
-        Some(SimulatedShot {
-            end_position: current_position,
-            step_count: step,
-            highest_y_position_reached: highest_y_reached,
-        })
-    } else {
-        None
+    SimulatedShot {
+        start_position: *start_position,
+        initial_velocity: *initial_velocity,
+        target_area: *target_area,
+        mid_positions,
+        end_position: if target_area.contains(&current_position) {
+            Some(current_position)
+        } else {
+            None
+        },
     }
 }
 
@@ -182,11 +243,111 @@ pub enum TargetAreaFromStrError {
     NotJustMinAndMaxValues(usize),
 }
 
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 struct SimulatedShot {
-    end_position: Position,
-    step_count: u128,
-    highest_y_position_reached: i128,
+    start_position: Position,
+    initial_velocity: Velocity,
+    target_area: TargetArea,
+    mid_positions: Vec<Position>,
+    end_position: Option<Position>,
+}
+
+impl SimulatedShot {
+    fn was_successful(&self) -> bool {
+        self.end_position.is_some()
+    }
+
+    fn highest_y_position_reached(&self) -> i128 {
+        let mid_positions_max = self
+            .mid_positions
+            .iter()
+            .map(|pos| pos.y)
+            .max()
+            .unwrap_or(self.start_position.y);
+        self.start_position.y.max(match &self.end_position {
+            None => mid_positions_max,
+            Some(end_position) => end_position.y.max(mid_positions_max),
+        })
+    }
+}
+
+impl std::fmt::Display for SimulatedShot {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let min_x = self
+            .mid_positions
+            .iter()
+            .map(|p| p.x)
+            .min()
+            .unwrap_or(self.start_position.x)
+            .min(self.start_position.x)
+            .min(self.target_area.min_x)
+            .min(self.target_area.max_x);
+        let min_x = match &self.end_position {
+            None => min_x,
+            Some(end_position) => min_x.min(end_position.x),
+        };
+
+        let max_x = self
+            .mid_positions
+            .iter()
+            .map(|p| p.x)
+            .max()
+            .unwrap_or(self.start_position.x)
+            .max(self.start_position.x)
+            .max(self.target_area.min_x)
+            .max(self.target_area.max_x);
+        let max_x = match &self.end_position {
+            None => max_x,
+            Some(end_position) => max_x.max(end_position.x),
+        };
+
+        let min_y = self
+            .mid_positions
+            .iter()
+            .map(|p| p.y)
+            .min()
+            .unwrap_or(self.start_position.y)
+            .min(self.start_position.y)
+            .min(self.target_area.min_y)
+            .min(self.target_area.max_y);
+        let min_y = match &self.end_position {
+            None => min_y,
+            Some(end_position) => min_y.min(end_position.y),
+        };
+
+        let max_y = self
+            .mid_positions
+            .iter()
+            .map(|p| p.y)
+            .max()
+            .unwrap_or(self.start_position.y)
+            .max(self.start_position.y)
+            .max(self.target_area.min_y)
+            .max(self.target_area.max_y);
+        let max_y = match &self.end_position {
+            None => max_y,
+            Some(end_position) => max_y.max(end_position.y),
+        };
+
+        for y in (min_y..=max_y).rev() {
+            for x in min_x..=max_x {
+                let position = Position { x, y };
+                if self.start_position == position {
+                    write!(f, "S")?;
+                } else if self.end_position.map(|p| p == position).unwrap_or(false)
+                    || self.mid_positions.iter().any(|p| *p == position)
+                {
+                    write!(f, "#")?;
+                } else if self.target_area.contains(&position) {
+                    write!(f, "T")?;
+                } else {
+                    write!(f, ".")?;
+                }
+            }
+            writeln!(f)?;
+        }
+        writeln!(f)
+    }
 }
 
 #[cfg(test)]
@@ -203,6 +364,152 @@ mod tests {
 
         // then
         assert_eq!(highest_y_position_possible, Ok(45));
+    }
+
+    #[test]
+    fn test_count_distinct_initial_velocities() {
+        // given
+        let input = "target area: x=20..30, y=-10..-5\r\n";
+
+        // when
+        let count_of_distinct_initial_velocities = count_distinct_initial_velocities(input);
+
+        // then
+        assert_eq!(count_of_distinct_initial_velocities, Ok(112));
+    }
+
+    #[test]
+    fn test_get_all_p() {
+        // given
+        let input = "target area: x=20..30, y=-10..-5\r\n";
+
+        // when
+        let got = get_all_p(&TargetArea::from_str(input).unwrap())
+            .into_iter()
+            .map(|simulated_shot| simulated_shot.initial_velocity)
+            .collect::<Vec<Velocity>>();
+
+        // then
+        let mut expected = {
+            vec![
+                Velocity { x: 23, y: -10 },
+                Velocity { x: 25, y: -9 },
+                Velocity { x: 27, y: -5 },
+                Velocity { x: 29, y: -6 },
+                Velocity { x: 22, y: -6 },
+                Velocity { x: 21, y: -7 },
+                Velocity { x: 9, y: 0 },
+                Velocity { x: 27, y: -7 },
+                Velocity { x: 24, y: -5 },
+                Velocity { x: 25, y: -7 },
+                Velocity { x: 26, y: -6 },
+                Velocity { x: 25, y: -5 },
+                Velocity { x: 6, y: 8 },
+                Velocity { x: 11, y: -2 },
+                Velocity { x: 20, y: -5 },
+                Velocity { x: 29, y: -10 },
+                Velocity { x: 6, y: 3 },
+                Velocity { x: 28, y: -7 },
+                Velocity { x: 8, y: 0 },
+                Velocity { x: 30, y: -6 },
+                Velocity { x: 29, y: -8 },
+                Velocity { x: 20, y: -10 },
+                Velocity { x: 6, y: 7 },
+                Velocity { x: 6, y: 4 },
+                Velocity { x: 6, y: 1 },
+                Velocity { x: 14, y: -4 },
+                Velocity { x: 21, y: -6 },
+                Velocity { x: 26, y: -10 },
+                Velocity { x: 7, y: -1 },
+                Velocity { x: 7, y: 7 },
+                Velocity { x: 8, y: -1 },
+                Velocity { x: 21, y: -9 },
+                Velocity { x: 6, y: 2 },
+                Velocity { x: 20, y: -7 },
+                Velocity { x: 30, y: -10 },
+                Velocity { x: 14, y: -3 },
+                Velocity { x: 20, y: -8 },
+                Velocity { x: 13, y: -2 },
+                Velocity { x: 7, y: 3 },
+                Velocity { x: 28, y: -8 },
+                Velocity { x: 29, y: -9 },
+                Velocity { x: 15, y: -3 },
+                Velocity { x: 22, y: -5 },
+                Velocity { x: 26, y: -8 },
+                Velocity { x: 25, y: -8 },
+                Velocity { x: 25, y: -6 },
+                Velocity { x: 15, y: -4 },
+                Velocity { x: 9, y: -2 },
+                Velocity { x: 15, y: -2 },
+                Velocity { x: 12, y: -2 },
+                Velocity { x: 28, y: -9 },
+                Velocity { x: 12, y: -3 },
+                Velocity { x: 24, y: -6 },
+                Velocity { x: 23, y: -7 },
+                Velocity { x: 25, y: -10 },
+                Velocity { x: 7, y: 8 },
+                Velocity { x: 11, y: -3 },
+                Velocity { x: 26, y: -7 },
+                Velocity { x: 7, y: 1 },
+                Velocity { x: 23, y: -9 },
+                Velocity { x: 6, y: 0 },
+                Velocity { x: 22, y: -10 },
+                Velocity { x: 27, y: -6 },
+                Velocity { x: 8, y: 1 },
+                Velocity { x: 22, y: -8 },
+                Velocity { x: 13, y: -4 },
+                Velocity { x: 7, y: 6 },
+                Velocity { x: 28, y: -6 },
+                Velocity { x: 11, y: -4 },
+                Velocity { x: 12, y: -4 },
+                Velocity { x: 26, y: -9 },
+                Velocity { x: 7, y: 4 },
+                Velocity { x: 24, y: -10 },
+                Velocity { x: 23, y: -8 },
+                Velocity { x: 30, y: -8 },
+                Velocity { x: 7, y: 0 },
+                Velocity { x: 9, y: -1 },
+                Velocity { x: 10, y: -1 },
+                Velocity { x: 26, y: -5 },
+                Velocity { x: 22, y: -9 },
+                Velocity { x: 6, y: 5 },
+                Velocity { x: 7, y: 5 },
+                Velocity { x: 23, y: -6 },
+                Velocity { x: 28, y: -10 },
+                Velocity { x: 10, y: -2 },
+                Velocity { x: 11, y: -1 },
+                Velocity { x: 20, y: -9 },
+                Velocity { x: 14, y: -2 },
+                Velocity { x: 29, y: -7 },
+                Velocity { x: 13, y: -3 },
+                Velocity { x: 23, y: -5 },
+                Velocity { x: 24, y: -8 },
+                Velocity { x: 27, y: -9 },
+                Velocity { x: 30, y: -7 },
+                Velocity { x: 28, y: -5 },
+                Velocity { x: 21, y: -10 },
+                Velocity { x: 7, y: 9 },
+                Velocity { x: 6, y: 6 },
+                Velocity { x: 21, y: -5 },
+                Velocity { x: 27, y: -10 },
+                Velocity { x: 7, y: 2 },
+                Velocity { x: 30, y: -9 },
+                Velocity { x: 21, y: -8 },
+                Velocity { x: 22, y: -7 },
+                Velocity { x: 24, y: -9 },
+                Velocity { x: 20, y: -6 },
+                Velocity { x: 6, y: 9 },
+                Velocity { x: 29, y: -5 },
+                Velocity { x: 8, y: -2 },
+                Velocity { x: 27, y: -8 },
+                Velocity { x: 30, y: -5 },
+                Velocity { x: 24, y: -7 },
+            ]
+        };
+        for n in got {
+            expected.retain(|v| *v != n);
+        }
+        assert_eq!(expected, Vec::new());
     }
 
     #[test]
@@ -223,11 +530,20 @@ mod tests {
         // then
         assert_eq!(
             simulation_result,
-            Some(SimulatedShot {
-                end_position: Position { x: 28, y: -7 },
-                step_count: 7,
-                highest_y_position_reached: 3
-            })
+            SimulatedShot {
+                start_position,
+                initial_velocity,
+                target_area,
+                mid_positions: vec![
+                    Position { x: 7, y: 2 },
+                    Position { x: 13, y: 3 },
+                    Position { x: 18, y: 3 },
+                    Position { x: 22, y: 2 },
+                    Position { x: 25, y: 0 },
+                    Position { x: 27, y: -3 },
+                ],
+                end_position: Some(Position { x: 28, y: -7 }),
+            }
         );
     }
 
@@ -249,11 +565,22 @@ mod tests {
         // then
         assert_eq!(
             simulation_result,
-            Some(SimulatedShot {
-                end_position: Position { x: 21, y: -9 },
-                step_count: 9,
-                highest_y_position_reached: 6
-            })
+            SimulatedShot {
+                start_position,
+                initial_velocity,
+                target_area,
+                mid_positions: vec![
+                    Position { x: 6, y: 3 },
+                    Position { x: 11, y: 5 },
+                    Position { x: 15, y: 6 },
+                    Position { x: 18, y: 6 },
+                    Position { x: 20, y: 5 },
+                    Position { x: 21, y: 3 },
+                    Position { x: 21, y: 0 },
+                    Position { x: 21, y: -4 },
+                ],
+                end_position: Some(Position { x: 21, y: -9 }),
+            }
         );
     }
 
@@ -275,11 +602,17 @@ mod tests {
         // then
         assert_eq!(
             simulation_result,
-            Some(SimulatedShot {
-                end_position: Position { x: 30, y: -6 },
-                step_count: 4,
-                highest_y_position_reached: 0
-            })
+            SimulatedShot {
+                start_position,
+                initial_velocity,
+                target_area,
+                mid_positions: vec![
+                    Position { x: 9, y: 0 },
+                    Position { x: 17, y: -1 },
+                    Position { x: 24, y: -3 },
+                ],
+                end_position: Some(Position { x: 30, y: -6 }),
+            }
         );
     }
 
@@ -299,6 +632,15 @@ mod tests {
         let simulation_result = simulate_shot(&start_position, &initial_velocity, &target_area);
 
         // then
-        assert_eq!(simulation_result, None);
+        assert_eq!(
+            simulation_result,
+            SimulatedShot {
+                start_position,
+                initial_velocity,
+                target_area,
+                mid_positions: vec![Position { x: 17, y: -4 },],
+                end_position: None,
+            }
+        );
     }
 }
